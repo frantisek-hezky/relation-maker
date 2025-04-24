ffc3f12a44cf32eb51a4a52769c319fe7b643d09
frantisek-hezky <hezkyf1@gmail.com>
2025-04-24 09:19:36 +0200

    darkmode

--- relationmaker.py
+++ relationmaker.py
@@ -55,5 +55,36 @@ if st.button("Vytvoř relace"):
 
     else:
         relace_maker()
-       
-   
+
+
+
+dark = '''
+<style>
+    .stApp {
+    background-color: black;
+    }
+</style>
+'''
+
+light = '''
+<style>
+    .stApp {
+    background-color: white;
+    }
+</style>
+'''
+
+st.markdown(light, unsafe_allow_html=True)
+toggle = st.button("Toggle theme")
+if toggle:
+    if st.session_state.theme == "light":
+        st.session_state.theme = "dark"
+    else:
+        st.session_state.theme = "light"
+
+# Apply the theme to the app
+if st.session_state.theme == "dark":
+    st.markdown(dark, unsafe_allow_html=True)
+else:
+    st.markdown(light, unsafe_allow_html=True)
+
